61f8d69a446e6c5fd2f576e282881192bc20c3a3
Ravindu Lakshan <rlgalathura@gmail.com>
2025-07-02 14:27:24 +0530

    Update PersonalInformation.jsx
    import the personalinformations css file

--- frontend/src/Pages/PersonalInformations/PersonalInformation.jsx
+++ frontend/src/Pages/PersonalInformations/PersonalInformation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
+import './PersonalInformation.css';
 
 function PersonalInformation() {
   return (
