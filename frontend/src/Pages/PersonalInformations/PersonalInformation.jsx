71884bf1595a86433523e52dc459dbaeacf42c3a
Ravindu Lakshan <rlgalathura@gmail.com>
2025-07-02 14:14:25 +0530

    Create PersonalInformation.jsx
    add new page to get user informations

---
+++ frontend/src/Pages/PersonalInformations/PersonalInformation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+
+function PersonalInformation() {
+  return (
+    <>
+      <Header />
+      <div className="personal-info-container">
+        <h1>Personal Information</h1>
+        {/* Add your actual content layout here */}
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default PersonalInformation;
