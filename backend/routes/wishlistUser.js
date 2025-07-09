a44fbef5d3af32baeeeda5b9d682062add534211
imasha <90449196+imashasew@users.noreply.github.com>
2025-07-09 06:31:35 +0530

    Update wishlistUser.js-commit 9

--- backend/routes/wishlistUser.js
+++ backend/routes/wishlistUser.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { getWishlist, addToWishlist, removeFromWishlist } = require('../controllers/wishlist.controller');
+
 // const { authenticateJWT } = require('../controllers/auth.controller');
 
 router.get('/', getWishlist);
 router.post('/:productId', addToWishlist);
 router.delete('/:productId', removeFromWishlist);
+
+const { authenticateJWT } = require('../controllers/auth.controller');
+const { getWishlistByUserId } = require('../controllers/wishlist.controller');
+
+
+router.get('/', authenticateJWT, getWishlist);
+router.post('/:productId', addToWishlist);
+router.delete('/:productId', removeFromWishlist);
+router.get('/user/:userId', getWishlistByUserId);
+
+
 
 module.exports = router; 
