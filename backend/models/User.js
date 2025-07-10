b74920d088992e174abb0f93df877a4b19106628
Ravindu Lakshan <rlgalathura@gmail.com>
2025-07-10 13:33:32 +0530

    Update User.js
    update for reset password

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
   password: { type: String },
   googleId: { type: String }, // optional
   wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }], // ✅ inside the object
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date }
 });
 
 module.exports = mongoose.model('User', userSchema);
-
