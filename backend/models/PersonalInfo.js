04a6c5e22c05566371e3d9d19922aab5fbe619e3
imasha <90449196+imashasew@users.noreply.github.com>
2025-07-11 02:41:38 +0530

    Update PersonalInfo.js-commit 10

--- backend/models/PersonalInfo.js
+++ backend/models/PersonalInfo.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const personalInfoSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   country: { type: String, required: true },
