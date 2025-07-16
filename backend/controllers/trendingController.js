5ada088ebda380f96213cd2085a4abd4d8be73fd
Ravindu Lakshan <rlgalathura@gmail.com>
2025-07-16 15:00:45 +0530

    Update trendingController.js
    update for filters

--- backend/controllers/trendingController.js
+++ backend/controllers/trendingController.js
@@ -1,4 +1,4 @@
-const TrendingProduct = require('../models/trendingModel');
+const TrendingProduct = require('../models/trendingModel'); 
 
 // GET all trending products
 exports.getAllTrendingProducts = async (req, res) => {
@@ -18,5 +18,31 @@ exports.createTrendingProduct = async (req, res) => {
     res.status(201).json(newProduct);
   } catch (err) {
     res.status(400).json({ error: err.message });
+  }
+};
+
+// NEW: Search trending products by name (case-insensitive, partial match)
+exports.searchTrendingProducts = async (req, res) => {
+  try {
+    const query = req.query.query || '';
+    if (!query) {
+      return res.status(400).json({ success: false, message: 'Query parameter is required' });
+    }
+
+    const products = await TrendingProduct.find({
+      name: { $regex: query, $options: 'i' }
+    });
+
+    res.json({
+      success: true,
+      count: products.length,
+      data: products
+    });
+  } catch (err) {
+    console.error('Error searching trending products:', err);
+    res.status(500).json({
+      success: false,
+      error: 'Internal server error'
+    });
   }
 };
